b00ce51b8e34f6a2234b5f4c27307502f611b87f
Damilola Jerugba <damiisdandy@icloud.com>
2021-12-25 14:54:36 +0100

    started pevious works

---
+++ src/pages/my-works.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyWorks = () => {
+  return (
+    <div className="MyWorks">
+      <h1 className="header">work showcase</h1>
+      <p className="desc">Here is a gallery of my previous works</p>
+    </div>
+  );
+};
+
+export default MyWorks;
